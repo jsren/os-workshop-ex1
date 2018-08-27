8128d774631d973bebd2e8a42c98d7d541a8cc8d
James Renwick <jsrenwick3@gmail.com>
2018-08-27 19:27:37 +0100

    Update makefiles for gcc 8.2

--- install-cygwin.mk
+++ install-cygwin.mk
@@ -7,79 +7,30 @@
 #
 # CYGWIN-SPECIFIC: removed 'sudo'
 
-
 # The install directory (do not change!)
 export PREFIX ?= /usr/os-workshop-gcc
 # The target architecture
 export TARGET ?= i686-elf
 # The version of GCC to build
-export TARGET_GCC ?= gcc-7.3.0
+export TARGET_GCC ?= gcc-8.2.0
 # The version of binutils to build
-export TARGET_BINUTILS ?= binutils-2.30
+export TARGET_BINUTILS ?= binutils-2.31
+# The directory in which to build
+export BUILD_DIR ?= build
 
 export PATH := $(PREFIX)/bin:$(PATH)
 SRC_APT := g++ bison flex libgmp3-dev libmpfr-dev libmpc-dev texinfo
 
-.PHONY: bootstrap setup build-all gcc binutils clean-all clean-binutils clean-gcc uninstall
+.PHONY: bootstrap build-all gcc binutils clean-all clean-binutils clean-gcc uninstall
 
-bootstrap: setup
+bootstrap: $(PREFIX)
 	#sudo apt install $(SRC_APT) # apt not supported by cygwin
 	mkdir -p build
-	cd build && $(MAKE) -j 2 -f $(abspath $(CURDIR)/install.mk) build-all
+	$(MAKE) -C build -j 2 -f $(abspath $(CURDIR)/install.mk) build-all
 
-setup: $(PREFIX)
+$(PREFIX):
 	mkdir -p $(PREFIX)
-	chmod -R a+rwx /usr/os-workshop-gcc/
+	chmod -R a+rwx $(PREFIX)
 
-$(TARGET_GCC).tar.gz:
-	wget ftp://ftp.gnu.org/gnu/gcc/$(TARGET_GCC)/$(TARGET_GCC).tar.gz
-
-$(TARGET_BINUTILS).tar.gz:
-	wget ftp://ftp.gnu.org/gnu/binutils/$(TARGET_BINUTILS).tar.gz
-
-$(TARGET_GCC)/README: $(TARGET_GCC).tar.gz
-	tar -xzf $(TARGET_GCC).tar.gz
-
-$(TARGET_BINUTILS)/README: $(TARGET_BINUTILS).tar.gz
-	tar -xzf $(TARGET_BINUTILS).tar.gz
-
-build-binutils/Makefile: $(TARGET_BINUTILS)/README
-	mkdir -p build-binutils
-	cd build-binutils && ../$(TARGET_BINUTILS)/configure --target=$(TARGET) --prefix="$(PREFIX)" --with-sysroot --disable-nls --disable-werror
-
-$(PREFIX)/bin/$(TARGET)-as: build-binutils/Makefile
-	cd build-binutils && $(MAKE) -j 2
-	cd build-binutils && $(MAKE) -j 2 install
-
-build-gcc/Makefile: $(PREFIX)/bin/$(TARGET)-as $(TARGET_GCC)/README
-	mkdir build-gcc
-	cd build-gcc && ../$(TARGET_GCC)/configure --target=$(TARGET) --prefix="$(PREFIX)" --disable-nls --enable-languages=c,c++ --without-headers
-
-$(PREFIX)/bin/$(TARGET)-gcc: build-gcc/Makefile
-	which -- $(TARGET)-as || { echo "Target binutils not in path" && exit 1; }
-	cd build-gcc && $(MAKE) -j 2 all-gcc
-	cd build-gcc && $(MAKE) -j 2 all-target-libgcc
-	cd build-gcc && $(MAKE) -j 2 install-gcc
-	cd build-gcc && $(MAKE) -j 2 install-target-libgcc
-
-binutils: $(PREFIX)/bin/$(TARGET)-as
-
-gcc: $(PREFIX)/bin/$(TARGET)-gcc
-
-clean-binutils:
-	rm -rf build/build-binutils
-clean-gcc:
-	rm -rf build/build-gcc
-clean-all:
-	rm -rf build
-
-build-all: gcc
-	$(TARGET)-gcc --version
-
-	@echo ==== Build Complete ====
-	whereis $(TARGET)-gcc
-
-	rm -rf build-binutils build-gcc $(TARGET_BINUTILS) $(TARGET_GCC)
-
-uninstall: clean-all
-	rm -rf /usr/os-workshop-gcc/
+%:
+	$(MAKE) -f $(abspath $(CURDIR)/install.mk) $@
--- install.mk
+++ install.mk
@@ -10,65 +10,74 @@ export PREFIX ?= /usr/os-workshop-gcc
 # The target architecture
 export TARGET ?= i686-elf
 # The version of GCC to build
-export TARGET_GCC ?= gcc-7.3.0
+export TARGET_GCC ?= gcc-8.2.0
 # The version of binutils to build
-export TARGET_BINUTILS ?= binutils-2.30
+export TARGET_BINUTILS ?= binutils-2.31
+# The directory in which to build
+export BUILD_DIR ?= build
 
 export PATH := $(PREFIX)/bin:$(PATH)
-SRC_APT := g++ bison flex libgmp3-dev libmpfr-dev libmpc-dev texinfo
+SRC_APT := g++ python xz-utils bison flex libgmp3-dev libmpfr-dev libmpc-dev texinfo
 
-.PHONY: bootstrap setup build-all gcc binutils clean-all clean-binutils clean-gcc uninstall
+.PHONY: bootstrap build-all gcc binutils clean-all clean-binutils clean-gcc uninstall
 
-bootstrap: setup
+bootstrap: $(PREFIX)
 	sudo apt install $(SRC_APT)
-	mkdir -p build
-	cd build && $(MAKE) -j 2 -f $(abspath $(CURDIR)/install.mk) build-all
+	mkdir -p $(BUILD_DIR)
+	$(MAKE) -C $(BUILD_DIR) -j 2 -f $(abspath $(CURDIR)/install.mk) build-all
 
-setup: $(PREFIX)
+$(PREFIX):
 	sudo mkdir -p $(PREFIX)
-	sudo chmod -R a+rwx /usr/os-workshop-gcc/
+	sudo chmod -R a+rwx $(PREFIX)
 
-$(TARGET_GCC).tar.gz:
-	wget ftp://ftp.gnu.org/gnu/gcc/$(TARGET_GCC)/$(TARGET_GCC).tar.gz
+$(TARGET_GCC).tar.xz:
+	wget https://ftp.gnu.org/gnu/gcc/$(TARGET_GCC)/$(TARGET_GCC).tar.xz
 
-$(TARGET_BINUTILS).tar.gz:
-	wget ftp://ftp.gnu.org/gnu/binutils/$(TARGET_BINUTILS).tar.gz
+$(TARGET_BINUTILS).tar.xz:
+	wget https://ftp.gnu.org/gnu/binutils/$(TARGET_BINUTILS).tar.xz
 
-$(TARGET_GCC)/README: $(TARGET_GCC).tar.gz
-	tar -xzf $(TARGET_GCC).tar.gz
+$(TARGET_GCC): $(TARGET_GCC).tar.xz
+	#tar -xJf $(TARGET_GCC).tar.xz
+	echo Tried to untar again!
 
-$(TARGET_BINUTILS)/README: $(TARGET_BINUTILS).tar.gz
-	tar -xzf $(TARGET_BINUTILS).tar.gz
+$(TARGET_BINUTILS): $(TARGET_BINUTILS).tar.xz
+	#tar -xJf $(TARGET_BINUTILS).tar.xz
+	echo Tried to untar again!
 
-build-binutils/Makefile: $(TARGET_BINUTILS)/README
+build-binutils/Makefile: $(TARGET_BINUTILS)
 	mkdir -p build-binutils
 	cd build-binutils && ../$(TARGET_BINUTILS)/configure --target=$(TARGET) --prefix="$(PREFIX)" --with-sysroot --disable-nls --disable-werror
 
 $(PREFIX)/bin/$(TARGET)-as: build-binutils/Makefile
-	cd build-binutils && $(MAKE) -j 2
-	cd build-binutils && $(MAKE) -j 2 install
+	cd build-binutils && $(MAKE) -j 8
+	cd build-binutils && $(MAKE) -j 8 install
 
-build-gcc/Makefile: $(PREFIX)/bin/$(TARGET)-as $(TARGET_GCC)/README
+build-gcc/Makefile: $(PREFIX)/bin/$(TARGET)-as $(TARGET_GCC)
+	# First patch configuration when building for x64
+ifeq ($(TARGET),x86_64-elf)
+	python $(dir $(abspath $(MAKEFILE_LIST)))/patch64.py $(TARGET_GCC)/gcc/config.gcc
+	printf "MULTILIB_OPTIONS += mno-red-zone\nMULTILIB_DIRNAMES += no-red-zone" > $(TARGET_GCC)/gcc/config/i386/t-x86_64-elf
+endif
 	mkdir build-gcc
 	cd build-gcc && ../$(TARGET_GCC)/configure --target=$(TARGET) --prefix="$(PREFIX)" --disable-nls --enable-languages=c,c++ --without-headers
 
 $(PREFIX)/bin/$(TARGET)-gcc: build-gcc/Makefile
 	which -- $(TARGET)-as || { echo "Target binutils not in path" && exit 1; }
-	cd build-gcc && $(MAKE) -j 2 all-gcc
-	cd build-gcc && $(MAKE) -j 2 all-target-libgcc
-	cd build-gcc && $(MAKE) -j 2 install-gcc
-	cd build-gcc && $(MAKE) -j 2 install-target-libgcc
+	cd build-gcc && $(MAKE) -j 8 all-gcc
+	cd build-gcc && $(MAKE) -j 8 all-target-libgcc
+	cd build-gcc && $(MAKE) -j 8 install-gcc
+	cd build-gcc && $(MAKE) -j 8 install-target-libgcc
 
-binutils: setup $(PREFIX)/bin/$(TARGET)-as
+binutils: $(PREFIX) $(PREFIX)/bin/$(TARGET)-as
 
-gcc: setup $(PREFIX)/bin/$(TARGET)-gcc
+gcc: $(PREFIX) $(PREFIX)/bin/$(TARGET)-gcc
 
 clean-binutils:
-	rm -rf build/build-binutils
+	rm -rf $(BUILD_DIR)/build-binutils
 clean-gcc:
-	rm -rf build/build-gcc
+	rm -rf $(BUILD_DIR)/build-gcc
 clean-all:
-	rm -rf build
+	rm -rf $(BUILD_DIR)
 
 build-all: gcc
 	$(TARGET)-gcc --version
@@ -79,4 +88,4 @@ build-all: gcc
 	rm -rf build-binutils build-gcc $(TARGET_BINUTILS) $(TARGET_GCC)
 
 uninstall: clean-all
-	rm -rf /usr/os-workshop-gcc/
+	rm -rf $(PREFIX)
